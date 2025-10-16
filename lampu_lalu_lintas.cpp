a1bbd5fa8bee31c262de60035c45914204925cbd
Fuad <93533757+fuadxtps23@users.noreply.github.com>
2025-10-16 21:00:18 +0700

    nambahin komen

--- lampu_lalu_lintas.cpp
+++ lampu_lalu_lintas.cpp
@@ -1,8 +1,10 @@
+// seperti biasa include library
 #include <iostream>
-
+// biar gak pake std std an yang banyak
 using namespace std;
-
+// fungsi utama disini
 int main() {
+	// mendeklarasikan variabel pilihan dengan tipe data intreger
 	int pilihan;
 	cout << "Menentukan lampu lalu lintas" << endl;
 	cout << "Masukan Lampu :" << endl;
@@ -10,7 +12,9 @@ int main() {
 	cout << "2.Merah" << endl;
 	cout << "3.Orange" << endl;
 	cout << "Masukan status lampu=";
+	// user diminta memasukan angka
 	cin >> pilihan;
+	// aplikasi memilih jawaban
 	if(pilihan == 1) {
 		cout << "Hijau. Majuuu!!" << endl;
 	} else if (pilihan == 2) {
@@ -18,5 +22,6 @@ int main() {
 	} else {
 		cout << "Orange. Hati Hati!!" << endl;
 	}
+	// kode berjalan sukses
 	return 0;
 }
